27298721efb729b8fc34ac12206824fcdd8ed73e
Wanderson Ferreira <wand@hey.com>
2021-03-12 18:57:52 -0300

    custom

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/arch/zettelkasten/20201215010543-informations.org"))
  '(package-selected-packages
-   '(docker docker-compose-mode dockerfile-mode zeal-at-point org-roam typescript-mode clojure-mode flycheck yasnippet magit expand-region pinentry ob-clojure popup google-translate langtool uuidgen uuidgen-el org-download org-noter google-this clj-refactor neotree vterm which-key vscode-dark-plus-theme use-package tide smex quickrun projectile prettier-js paredit org-roam-server multiple-cursors move-text modus-themes lsp-ui lsp-java ledger-mode kaocha-runner jump-char java-snippets ido-completing-read+ forge fix-word doom-themes diminish company clojure-snippets change-inner add-node-modules-path))
+   '(zenburn-theme docker docker-compose-mode dockerfile-mode zeal-at-point org-roam typescript-mode clojure-mode flycheck yasnippet magit expand-region pinentry ob-clojure popup google-translate langtool uuidgen uuidgen-el org-download org-noter google-this clj-refactor neotree vterm which-key vscode-dark-plus-theme use-package tide smex quickrun projectile prettier-js paredit org-roam-server multiple-cursors move-text modus-themes lsp-ui lsp-java ledger-mode kaocha-runner jump-char java-snippets ido-completing-read+ forge fix-word doom-themes diminish company clojure-snippets change-inner add-node-modules-path))
  '(safe-local-variable-values
    '((tgt-projects
       ((:root-dir "/home/wand/platform/seu-barriga")
@@ -21,9 +21,3 @@
      (cider-ns-refresh-after-fn . "integrant.repl/resume")
      (cider-ns-refresh-before-fn . "integrant.repl/suspend")))
  '(warning-suppress-types '((comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "grey75" :foreground "black")))))
