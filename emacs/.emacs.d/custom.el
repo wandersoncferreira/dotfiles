4618c616228d3572813e233c6bd0bc74319943f8
Wanderson Ferreira <wand@hey.com>
2021-03-23 10:23:53 -0300

    custom files and better gitignore

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,33 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/arch/zettelkasten/20201215010543-informations.org"))
  '(package-selected-packages
-   '(toggle-test company-quickhelp popwin flycheck-projectile system-packages vlf ibuffer-vc browse-at-remote gitignore-templates gitignore-mode gitconfig-mode git-timemachine diff-hl bm pomidor gist rg sql-indent zenburn-theme docker docker-compose-mode dockerfile-mode zeal-at-point org-roam typescript-mode clojure-mode flycheck yasnippet magit expand-region pinentry ob-clojure popup google-translate langtool uuidgen uuidgen-el org-download org-noter google-this clj-refactor neotree vterm which-key vscode-dark-plus-theme use-package tide smex quickrun projectile prettier-js paredit org-roam-server multiple-cursors move-text modus-themes lsp-ui lsp-java ledger-mode kaocha-runner jump-char java-snippets ido-completing-read+ forge fix-word doom-themes diminish company clojure-snippets change-inner add-node-modules-path))
- '(safe-local-variable-values
-   '((tgt-projects
-      ((:root-dir "/home/wanderson/arch/study/parens-of-the-dead")
-       (:src-dirs "src")
-       (:test-dirs "test")
-       (:test-suffixes "_test")))
-     (cider-ns-refresh-before-fn . "user/stop")
-     (cider-ns-refresh-after-fn . "user/go")
-     (cider-known-endpoints
-      ("localhost" "8777"))
-     (cider-pprint-fn . zprint)
-     (tgt-projects
-      ((:root-dir "/home/wand/platform/seu-barriga")
-       (:src-dirs "src")
-       (:test-dirs "test")
-       (:test-suffixes "_test")))
-     (cider-docker-translations
-      ("/app/src" . "/home/wand/platform/seu-barriga/src")
-      ("/app/test" . "/home/wand/platform/seu-barriga/test"))
-     (cider-docker-translations
-      ("/app/src" . "/home/wand/secland/src")
-      ("/app/test" . "/home/wand/secland/test"))
-     (cider-ns-refresh-after-fn . "integrant.repl/resume")
-     (cider-ns-refresh-before-fn . "integrant.repl/suspend")))
+   '(nix-mode pomidor rg sql-indent docker docker-compose-mode dockerfile-mode ledger-mode google-this uuidgen org-roam-server org-roam org-download google-translate popup langtool which-key tide add-node-modules-path prettier-js typescript-mode lsp-java lsp-ui clj-refactor lsp-mode toggle-test kaocha-runner clojure-mode paredit popwin flycheck-projectile flycheck quickrun clojure-snippets java-snippets yasnippet company-quickhelp company gist browse-at-remote gitignore-templates gitignore-mode gitconfig-mode git-timemachine diff-hl forge magit move-text multiple-cursors jump-char fix-word change-inner expand-region zeal-at-point projectile vlf ibuffer-vc diminish use-package vterm))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
