ed7f70ce00104b3e4da5d23190e04a31906510af
Wanderson Ferreira <wand@hey.com>
2021-05-02 20:52:21 -0300

    [os] dual dell-27" monitors setup

---
+++ .screenlayout/workstation.sh
@@ -0,0 +1,1 @@
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
