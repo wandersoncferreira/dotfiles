1aa732ed61ba9ab7e0dd173238898d80f45c9ca3
Wanderson Ferreira <wand@hey.com>
2021-05-05 14:53:49 -0300

    [emacs] external services

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1682,6 +1682,62 @@ Better naming to improve the chances to find it."
   (run-at-time 10 nil #'appt-activate 1)
   (add-hook 'diary-hook 'appt-make-list))
 
+;;; Manage external services
+
+(use-package prodigy
+  :ensure t
+  :config
+  (prodigy-define-service
+    :name "tempo"
+    :command "make"
+    :cwd "~/repos/experiments/tempo/"
+    :stop-signal 'sigkill
+    :tags '(eas-tempo)
+    :kill-process-buffer-on-stop t)
+
+  (prodigy-define-service
+    :name "esource-repl"
+    :command "make"
+    :args '("repl")
+    :cwd "~/repos/reifyhealth/esource-service/"
+    :stop-signal 'sigkill
+    :tags '(esource-repl)
+    :kill-process-buffer-on-stop t)
+
+  (prodigy-define-service
+    :name "esource"
+    :command "make"
+    :args '("run")
+    :cwd "~/repos/reifyhealth/esource-service/"
+    :stop-signal 'sigkill
+    :tags '(esource-run)
+    :kill-process-buffer-on-stop t)
+
+  (prodigy-define-service
+    :name "study-sheet"
+    :command "yarn"
+    :args '("start")
+    :cwd "~/repos/reifyhealth/study-sheet/"
+    :stop-signal 'sigkill
+    :tags '(stdocs)
+    :kill-process-buffer-on-stop t)
+
+  (prodigy-define-tag
+    :name 'eas-tempo
+    :ready-message ".*Build completed.*")
+
+  (prodigy-define-tag
+    :name 'esource-repl
+    :ready-message "nREPL started!")
+
+  (prodigy-define-tag
+    :name 'stdocs
+    :ready-message "Starting the development server...")
+
+  (prodigy-define-tag
+    :name 'esource-run
+    :ready-message "WARNING: .*"))
+
 ;;; End of file
 
 ;; Local Variables:
