0b9b4d9145131567f4060b84d7973304b4128c17
Wanderson Ferreira <wand@hey.com>
2021-05-13 12:33:27 -0300

    [emacs] early-init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,39 +6,6 @@
 
 ;;; Code:
 
-;;; Packages
-
-(require 'package)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(setq package-pinned-packages '())
-
-(package-initialize)
-
-(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (package-install 'dash)
-  (package-install 'diminish))
-
-(setq lisps-dir (expand-file-name "lisps" user-emacs-directory))
-(add-to-list 'load-path lisps-dir)
-
-(setq custom-theme-directory (concat user-emacs-directory "themes"))
-
-(dolist
-    (path (directory-files custom-theme-directory t "\\w+"))
-  (when (file-directory-p path)
-    (add-to-list 'custom-theme-load-path path)))
-
-(defun bk/reinstall-package (pkg)
-  "Reinstall PKG that are not properly byte-compiled."
-  (interactive (list (intern (completing-read "Reinstall package: " (mapcar #'car package-alist)))))
-  (unload-feature pkg)
-  (package-reinstall pkg)
-  (require pkg))
-
 ;;; Security
 (eval-after-load "enriched"
   '(defun enriched-decode-display-prop (start end &optional param)
