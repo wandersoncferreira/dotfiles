26d5d04712ce2a137384401908ca271cdbf4415d
Wanderson Ferreira <wand@hey.com>
2021-07-11 20:13:30 -0300

    [emacs] add create pr as draft to forge.

--- .doom.d/+extra-vc.el
+++ .doom.d/+extra-vc.el
@@ -4,3 +4,19 @@
   (setq magit-log-show-gpg-status t
         magit-commit-show-diff nil
         magit-display-buffer-function (lambda (buf) (display-buffer buf '(display-buffer-same-window)))))
+
+(defun bk/forge--add-draft (alist)
+  "Add draft to ALIST."
+  (append alist '((draft . "t"))))
+
+(defun bk/create-draft-pull-request ()
+  "Submit the post that is being edit in the current buffer as a draft."
+  (interactive)
+  (advice-add 'forge--topic-parse-buffer
+              :filter-return #'bk/forge--add-draft)
+  (condition-case err
+      (forge-post-submit)
+    (t
+     (advice-remove 'forge--topic-parse-buffer #'bk/forge--add-draft)
+     (signal (car err) (cdr err))))
+  (advice-remove 'forge--topic-parse-buffer #'bk/forge--add-draft))
