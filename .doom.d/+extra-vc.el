855b11dce39d733f7855774b7f11b7d44f7ebe6b
Wanderson Ferreira <wand@hey.com>
2021-10-15 16:52:55 -0300

    [emacs] update vc

--- .doom.d/+extra-vc.el
+++ .doom.d/+extra-vc.el
@@ -1,15 +1,21 @@
 ;;; ../dotfiles/.doom.d/+extra-vc.el -*- lexical-binding: t; -*-
 
-(setq magit-diff-refine-hunk t
-      magit-section-initial-visibility-alist
-      `((untracked . show)
-        (unstaged . show)
-        (unpushed . show)
-        (unpulled . show)
-        (stashes . show)))
+(use-package! magit
+  :init
+  (setq magit-diff-refine-hunk t
+        magit-log-show-gpg-status t
+        magit-commit-show-diff nil
+        magit-display-buffer-function (lambda (buf) (display-buffer buf '(display-buffer-same-window)))
+        magit-section-initial-visibility-alist
+        `((untracked . show)
+          (unstaged . show)
+          (unpushed . show)
+          (unpulled . show)
+          (stashes . show))))
 
-(require 'bug-reference-github)
-(add-hook 'prog-mode-hook 'bug-reference-github-set-url-format)
+(use-package! bug-reference-github
+  :config
+  (add-hook 'prog-mode-hook 'bug-reference-github-set-url-format))
 
 (defun bk/forge--add-draft (alist)
   "Add draft to ALIST."
@@ -38,14 +44,3 @@
   (setq github-review-view-comments-in-code-lines t
         github-review-view-comments-in-code-lines-outdated t
         github-review-reply-inline-comments t))
-
-(map! "s-g" #'gh-notify
-
-      :map forge-post-mode-map
-      "C-c C-d" #'bk/post-draft-pull-request
-
-      :map diff-mode-map
-      "C-c s" #'bk/github-review--copy-suggestion
-
-      :map forge-topic-mode-map
-      "C-c r" #'github-review-forge-pr-at-point)
