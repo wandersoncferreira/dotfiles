60217ded5eab506f7497c928426e6444d3be5e4a
Wanderson Ferreira <wand@hey.com>
2021-12-21 18:41:45 -0300

    useful defadvices

--- .doom.d/+extra-defaults.el
+++ .doom.d/+extra-defaults.el
@@ -4,8 +4,13 @@
       user-mail-address "wand@hey.com"
 
       byte-compile-warnings '(cl-functions)
+
       scroll-margin 2
+      line-spacing 3
+
       enable-local-variables t
+      messages-buffer-max-lines 10000
+
       load-prefer-newer t
       show-help-function nil
       read-process-output-max (* 1024 1024)
@@ -29,3 +34,39 @@
           (defun +abbrev-file-name ()
             (setq-default abbrev-mode t)
             (setq abbrev-file-name (expand-file-name "abbrev.el" doom-private-dir))))
+
+(add-hook! 'next-error-hook #'recenter)
+
+
+(defadvice kill-line (before kill-line-autoreindent activate)
+  "Kill excess whitespace when joining lines.
+If the next line is joined to the current line, kill the extra indent whitespace."
+  (when (and (eolp) (not (bobp)))
+    (save-excursion
+      (forward-char 1)
+      (just-one-space 1))))
+
+(defadvice load (before debug-log activate)
+  (message "Advice: now loading: '%s'" (ad-get-arg 0)))
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Single line killed")
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no activate region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+(defadvice align-regexp (around align-regexp-with-spaces)
+  "Never use tabs for alignment."
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+
+(ad-activate 'align-regexp)
