5ed056cd6c1a3f7059e60af77e7325c47319b120
Wanderson Ferreira <wand@hey.com>
2021-12-22 10:33:12 -0300

    typescript

--- .doom.d/+extra-typescript.el
+++
@@ -1,4 +0,0 @@
-;;; ../dotfiles/.doom.d/+extra-typescript.el -*- lexical-binding: t; -*-
-
-(add-hook 'typescript-mode-hook #'format-all-mode)
-(setq-hook! 'typescript-mode-hook +format-with-lsp nil)
